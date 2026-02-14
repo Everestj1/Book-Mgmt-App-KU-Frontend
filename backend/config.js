9c5766a0016f821c24e71958c9278166907d2d3d
Everestj1 <108292817+Everestj1@users.noreply.github.com>
2026-02-14 10:26:24 +0500

    Delete backend directory

--- backend/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL = "mongodb+srv://ever888:Mongodb.1@mern-bookstore.t8rhyeh.mongodb.net/books-collection?retryWrites=true&w=majority&appName=mern-bookstore";
